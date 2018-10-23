5826d2fc6a3332fa7f38c38599fa8bef36be1462
Sharon Romero <sh-aron@hotmail.com>
2018-10-23 13:49:55 -0600

    Updated autoloader

--- php/Classes/Article.php
+++ php/Classes/Article.php
@@ -1,7 +1,7 @@
 <?php
-namespace sromero130\DataDesign;
+namespace sharonromero\DataDesign;
 
-require_once(dirname(__DIR__, 2) . "/Classes/autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
